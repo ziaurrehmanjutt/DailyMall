6a60cdeb271922edd5591e3bf569e9d195edcba3
Zia Ur Rehman <ziaa520@gmail.com>
2021-04-20 21:17:23 +0500

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+// import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy } ,InAppBrowser],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { DomSanitizer} from '@angular/platform-browser';
-import { Plugins } from '@capacitor/core';
-const { Browser } = Plugins;
+// import { DomSanitizer} from '@angular/platform-browser';
+// import {  Plugins } from '@capacitor/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+// const { Browser } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,17 @@ const { Browser } = Plugins;
 })
 export class HomePage {
 
-  constructor(private sanitizer: DomSanitizer) {
-    this.openUrl();
+  constructor(
+    // private sanitizer: DomSanitizer
+    private iab: InAppBrowser
+    ) {
+    // this.openUrl();
+    const browser = this.iab.create('https://www.dailymall.id/','_blank', 'location=no');
   }
 
   async openUrl(){
     console.log('Opening');
-    await Browser.open({ url: 'https://www.dailymall.id' });
+   // await Browser.open({ url: 'https://www.dailymall.id/' , windowName: '_self' , presentationStyle : 'fullscreen'});
   }
   // openURL(){
   //   return this.sanitizer.bypassSecurityTrustResourceUrl("https://www.dailymall.id/");
